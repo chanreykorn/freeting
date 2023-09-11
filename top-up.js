25a369e0f00432343a9687cce5ed14e24710cdb6
chanreykorn <139063062+chanreykorn@users.noreply.github.com>
2023-09-11 14:57:37 +0700

    Add files via upload

---
+++ top-up.js
@@ -0,0 +1,22 @@
+let toUp = document.getElementById("toUp");
+let user = document.getElementById("user");
+let closes = document.getElementById("closes");
+let formSearch = document.getElementById("form-search");
+let search = document.getElementById("search");
+
+// user.addEventListener("click", function(){
+//     if(user = 1){
+//         toUp.classList.add("active");
+//     }
+// });
+
+// search.addEventListener("click", function(){
+//     if(search = true){
+//         formSearch.classList.add("active");
+//     }
+// });
+// search.addEventListener("click", function(){
+//     if(search = false){
+//         formSearch.classList.remove("active");
+//     }
+// });
